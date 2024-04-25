027cd372c46721054a7bf0dc5562e7b4361761dd
user <user@LAPTOP-Q7J1A6BT>
2024-04-25 21:19:30 +0300

    Proje dosyası ekle.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using VIZYONDAKİLER.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace VIZYONDAKİLER.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+       
+
+        public IActionResult KoltukSecimi()
+        {
+            return View();
+        }
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/LoginPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VIZYONDAKİLER.Controllers
+{
+    public class LoginPageController : Controller
+    {
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+    }
+}
